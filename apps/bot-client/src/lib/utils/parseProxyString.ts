60988f90360a302adc10ced94ef8d06c6132c38b
galionix <galionix2@gmail.com>
2024-04-30 22:51:46 +0300

    use proxy

--- apps/bot-client/src/lib/utils/parseProxyString.ts
+++ apps/bot-client/src/lib/utils/parseProxyString.ts
@@ -4,9 +4,11 @@ export const parseProxyString = (proxyString: string): ProxyInterface => {
   // ip:port:user:password
   const [ip, port, user, password] = proxyString.split(":");
   return {
+    socksType: 5,
     ip,
     port: parseInt(port),
     username: user,
     password,
+    MTProxy: false,
   };
 };
